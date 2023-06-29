da340c152b00368ef04d64cadd69376c3d41e500
Ernesto Quezada <ernesto.quezada@gmail.com>
2023-06-29 08:59:39 -0600

    Background colour updated

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
 </head>
-<body style="display: flex; align-items: center; height: 100vh; background-color: #0c273a;">
+<body style="display: flex; align-items: center; height: 100vh; background-color: #ff7700;">
 	<div class="container text-center">
 		<div class="row">
 			<div class="col"></div>
