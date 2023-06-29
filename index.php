759008650cc9098370161e4e80948bcc79dbdf5a
Ernesto Quezada <ernesto.quezada@gmail.com>
2023-06-28 22:43:37 -0600

    Clean up

--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 <html>
 <head>
     <title>Flappy Cubird</title>
-	<!-- manuel3g -->
 	<link rel="icon" href="https://logodownload.org/wp-content/uploads/2017/06/bitcoin-logo-1-1.png">
-	<!-- manuel3g -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
 </head>
@@ -18,14 +16,12 @@
 					<h3 style="color:white;">Enter your data<br>to start the game</h3>
 					<br>
 					<div class="mb-3">
-						<!-- <label for="nombre" class="form-label">Nombre</label> -->
 						<input class="form-control" type="text" name="player" placeholder="Type your name" aria-label="default input" required>
 					</div>
 					<div class="mb-3">
-						<!-- <label for="puerto" class="form-label">Puerto</label> -->
 						<input class="form-control" type="text" name="port" placeholder="Type your port" aria-label="default input" required>
 					</div>
-					<button type="submit" class="btn btn-success" style="width: 100%;">Iniciar</button>
+					<button type="submit" class="btn btn-success" style="width: 100%;">Play Game</button>
 				</form>
 
 			</div>
